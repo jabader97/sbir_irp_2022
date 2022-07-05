[FIX, ADD] Fixed TU-Berlin dataloader, added accuracy calculation, removed redundant code for getting numeric classes"

src/data.py
@@ -13,7 +13,7 @@ from PIL import Image, ImageOps
 
 class DataGeneratorPaired(data.Dataset):
     def __init__(self, dataset, root, photo_dir, sketch_dir, photo_sd, sketch_sd, fls_sk, fls_im, clss,
-                 transforms_sketch=None, transforms_image=None, cid_mask=False, zero_version=''):
+                 transforms_sketch=None, transforms_image=None, int2str='', zero_version=''):
         self.dataset = dataset
         self.root = root
         self.photo_dir = photo_dir
@@ -25,12 +25,15 @@ class DataGeneratorPaired(data.Dataset):
         self.clss = clss
         self.transforms_sketch = transforms_sketch
         self.transforms_image = transforms_image
-        self.cid_mask = cid_mask
-        if cid_mask:
+        if len(int2str) > 0:
             cid_mask_file = os.path.join(self.root, zero_version, 'cid_mask.pickle')
             with open(cid_mask_file, 'rb') as fh:
                 int_cid_matrix = pickle.load(fh)
-            self.cid_matrix = self.get_class_name_from_int(int_cid_matrix, zero_version)
+            self.cid_matrix = {}
+            for key in int_cid_matrix.keys():
+                self.cid_matrix[int2str[key]] = int_cid_matrix[key]
+        else:
+            self.cid_matrix = ''
 
     def __getitem__(self, item):
         get_item_time = time.time()
@@ -42,7 +45,7 @@ class DataGeneratorPaired(data.Dataset):
             im = self.transforms_image(im)
         if self.transforms_sketch is not None:
             sk = self.transforms_sketch(sk)
-        if self.cid_mask:
+        if len(self.cid_matrix) > 0:
             mask = self.cid_matrix[cls]
             return sk, im, cls, mask, (time.time() - get_item_time)
         return sk, im, cls, (time.time() - get_item_time)
@@ -58,34 +61,10 @@ class DataGeneratorPaired(data.Dataset):
             weights[idx] = 1 / idx.shape[0]
         return weights
 
-    def get_class_name_from_int(self, int_cid_matrix, zero_version):
-        # load the conversion file
-        int_to_class_path = os.path.join(self.root, zero_version, 'cname_cid.txt')
-        int_to_class_dict = {}
-        with open(int_to_class_path) as f:
-            for line in f:
-                key, val = self.process_str(line)
-                int_to_class_dict[int(key)] = val
-        # convert the classes
-        class_cid_matrix = {}
-        for key in int_cid_matrix.keys():
-            class_cid_matrix[int_to_class_dict[key]] = int_cid_matrix[key]
-        return class_cid_matrix
-
-    @staticmethod
-    def process_str(line):
-        contents = line.split()
-        key = contents[-1]
-        name = []
-        for c in contents[0:-1]:
-            name += c.split('-')
-        join_symbol = '_'
-        val = join_symbol.join(name)
-        return key, val
-
 
 class DataGeneratorSketch(data.Dataset):
-    def __init__(self, dataset, root, sketch_dir, sketch_sd, fls_sk, clss_sk, transforms=None, cid_mask=False, zero_version=''):
+    def __init__(self, dataset, root, sketch_dir, sketch_sd, fls_sk, clss_sk, transforms=None, int2str='',
+                 zero_version=''):
         self.dataset = dataset
         self.root = root
         self.sketch_dir = sketch_dir
@@ -93,12 +72,15 @@ class DataGeneratorSketch(data.Dataset):
         self.fls_sk = fls_sk
         self.clss_sk = clss_sk
         self.transforms = transforms
-        self.cid_mask = cid_mask
-        if cid_mask:
+        if len(int2str) > 0:
             cid_mask_file = os.path.join(self.root, zero_version, 'cid_mask.pickle')
             with open(cid_mask_file, 'rb') as fh:
                 int_cid_matrix = pickle.load(fh)
-            self.cid_matrix = self.get_class_name_from_int(int_cid_matrix, zero_version)
+            self.cid_matrix = {}
+            for key in int_cid_matrix.keys():
+                self.cid_matrix[int2str[key]] = int_cid_matrix[key]
+        else:
+            self.cid_matrix = ''
 
     def __getitem__(self, item):
         get_item_time = time.time()
@@ -107,7 +89,7 @@ class DataGeneratorSketch(data.Dataset):
         cls_sk = self.clss_sk[item]
         if self.transforms is not None:
             sk = self.transforms(sk)
-        if self.cid_mask:
+        if len(self.cid_matrix) > 0:
             mask = self.cid_matrix[cls_sk]
             return sk, cls_sk, mask, (time.time() - get_item_time)
 
@@ -124,34 +106,10 @@ class DataGeneratorSketch(data.Dataset):
             weights[idx] = 1 / idx.shape[0]
         return weights
 
-    def get_class_name_from_int(self, int_cid_matrix, zero_version):
-        # load the conversion file
-        int_to_class_path = os.path.join(self.root, zero_version, 'cname_cid.txt')
-        int_to_class_dict = {}
-        with open(int_to_class_path) as f:
-            for line in f:
-                key, val = self.process_str(line)
-                int_to_class_dict[int(key)] = val
-        # convert the classes
-        class_cid_matrix = {}
-        for key in int_cid_matrix.keys():
-            class_cid_matrix[int_to_class_dict[key]] = int_cid_matrix[key]
-        return class_cid_matrix
-
-    @staticmethod
-    def process_str(line):
-        contents = line.split()
-        key = contents[-1]
-        name = []
-        for c in contents[0:-1]:
-            name += c.split('-')
-        join_symbol = '_'
-        val = join_symbol.join(name)
-        return key, val
-
 
 class DataGeneratorImage(data.Dataset):
-    def __init__(self, dataset, root, photo_dir, photo_sd, fls_im, clss_im, transforms=None, cid_mask=False, zero_version=''):
+    def __init__(self, dataset, root, photo_dir, photo_sd, fls_im, clss_im, transforms=None, int2str='',
+                 zero_version=''):
 
         self.dataset = dataset
         self.root = root
@@ -160,12 +118,15 @@ class DataGeneratorImage(data.Dataset):
         self.fls_im = fls_im
         self.clss_im = clss_im
         self.transforms = transforms
-        self.cid_mask = cid_mask
-        if cid_mask:
+        if len(int2str) > 0:
             cid_mask_file = os.path.join(self.root, zero_version, 'cid_mask.pickle')
             with open(cid_mask_file, 'rb') as fh:
                 int_cid_matrix = pickle.load(fh)
-            self.cid_matrix = self.get_class_name_from_int(int_cid_matrix, zero_version)
+            self.cid_matrix = {}
+            for key in int_cid_matrix.keys():
+                self.cid_matrix[int2str[key]] = int_cid_matrix[key]
+        else:
+            self.cid_matrix = ''
 
     def __getitem__(self, item):
         get_item_time = time.time()
@@ -173,7 +134,7 @@ class DataGeneratorImage(data.Dataset):
         cls_im = self.clss_im[item]
         if self.transforms is not None:
             im = self.transforms(im)
-        if self.cid_mask:
+        if len(self.cid_matrix) > 0:
             mask = self.cid_matrix[cls_im]
             return im, cls_im, mask, (time.time() - get_item_time)
         return im, cls_im, (time.time() - get_item_time)
@@ -188,29 +149,3 @@ class DataGeneratorImage(data.Dataset):
             idx = np.where(self.clss_im == cls)[0]
             weights[idx] = 1 / idx.shape[0]
         return weights
-
-    def get_class_name_from_int(self, int_cid_matrix, zero_version):
-        # load the conversion file
-        int_to_class_path = os.path.join(self.root, zero_version, 'cname_cid.txt')
-        int_to_class_dict = {}
-        with open(int_to_class_path) as f:
-            for line in f:
-                key, val = self.process_str(line)
-                int_to_class_dict[int(key)] = val
-        # convert the classes
-        class_cid_matrix = {}
-        for key in int_cid_matrix.keys():
-            class_cid_matrix[int_to_class_dict[key]] = int_cid_matrix[key]
-        return class_cid_matrix
-
-    @staticmethod
-    def process_str(line):
-        contents = line.split()
-        key = contents[-1]
-        name = []
-        for c in contents[0:-1]:
-            name += c.split('-')
-        join_symbol = '_'
-        val = join_symbol.join(name)
-        return key, val
-

src/models/sake.py
@@ -11,6 +11,7 @@ import os, time
 from logger import AverageMeter
 from models.senet import cse_resnet50, cse_resnet50_hashing
 from models.resnet import resnet50_hashing
+from utils import create_dict_texts
 
 
 class EMSLoss(nn.Module):
@@ -373,7 +374,7 @@ class SAKE(nn.Module):
         self.optimizer = torch.optim.Adam(self.model.parameters(), lr=self.lr,
                                           weight_decay=params_model['weight_decay'])
 
-        self.class_to_int_dict = self.get_class_int_from_str_dict()
+        self.class_to_int_dict, _ = create_dict_texts(self.root, self.zero_version)
         self.sake_lambda = params_model['sake_lambda']
         cudnn.benchmark = True
 
@@ -490,25 +491,20 @@ class SAKE(nn.Module):
         features = F.normalize(features)
         return features
 
-    def get_class_int_from_str_dict(self):
-        class_to_int_path = os.path.join(self.root, self.zero_version, 'cname_cid.txt')
-        class_to_int_dict = {}
-        with open(class_to_int_path) as f:
-            for line in f:
-                key, val = self.process_str(line)
-                class_to_int_dict[key] = val
-        return class_to_int_dict
+    def get_sketch_prediction(self, sk):
+        tag = torch.zeros(sk.size()[0], 1)
+        if torch.cuda.is_available():
+            tag = tag.cuda()
+        prediction, _ = self.model(sk, tag)
+        return prediction
 
-    @staticmethod
-    def process_str(line):
-        contents = line.split()
-        val = contents[-1]
-        name = []
-        for c in contents[0:-1]:
-            name += c.split('-')
-        join_symbol = '_'
-        key = join_symbol.join(name)
-        return key, val
+    def get_image_prediction(self, im):
+        tag = torch.ones(im.size()[0], 1)
+        if torch.cuda.is_available():
+            tag = tag.cuda()
+        prediction, _ = self.model(im, tag)
+        return prediction
+
 
     @staticmethod
     def add_to_log(logger, losses):

src/models/sem_pcyc.py
@@ -165,7 +165,7 @@ class SEM_PCYC(nn.Module):
                 new_dict['tablelamp'] = table_lamp
             self.sem.append(new_dict)
 
-        self.dict_clss = params_model['dict_clss']
+        self.dict_clss = params_model['dict_clss_str2int']
         print('Done')
 
         print('Initializing trainable models...', end='')
@@ -297,9 +297,7 @@ class SEM_PCYC(nn.Module):
                 if self.aut_enc.enc[l]._get_name() == 'Linear':
                     regularizer = regularizer + torch.norm(self.aut_enc.enc[l].weight, 2, dim=0).sum()
         loss_aut_enc = self.criterion_reg(self.se_em_rec, se) + self.lambda_regular * regularizer / cl.shape[0]
-        # self.optimizer_ae.zero_grad()
         loss_aut_enc.backward(retain_graph=True)
-        # self.optimizer_ae.step()
 
         # Adversarial loss with flipped labels (false -> true)
         loss_gen_adv = self.criterion_gan(self.disc_se(self.im2se_em), True) + \
@@ -331,13 +329,7 @@ class SEM_PCYC(nn.Module):
 
         # Sum the above generator losses for back propagation and displaying
         loss_gen = loss_gen_adv + loss_gen_cyc + loss_gen_cls + loss_gen_reg
-
-        # self.optimizer_gen.zero_grad()
         loss_gen.backward(retain_graph=True)
-        # self.optimizer_gen.step()
-
-        # initialize optimizer
-        # self.optimizer_disc.zero_grad()
 
         # Semantic discriminator loss
         loss_disc_se = 2 * self.criterion_gan(self.disc_se(self.se_em_enc), True) + \
@@ -376,7 +368,8 @@ class SEM_PCYC(nn.Module):
 
         # Get numeric classes
         numeric_class_time = time.time()
-        num_cls = torch.from_numpy(sem_utils.numeric_classes(cl, self.dict_clss))
+        temp = sem_utils.numeric_classes(cl, self.dict_clss)
+        num_cls = torch.LongTensor(sem_utils.numeric_classes(cl, self.dict_clss))
         if torch.cuda.is_available():
             num_cls = num_cls.cuda()
         self.time_info['numeric_class_time'].update(time.time() - numeric_class_time)
@@ -420,6 +413,12 @@ class SEM_PCYC(nn.Module):
         im_em = self.gen_im2se(self.image_model(im))
 
         return im_em
+
+    def get_sketch_prediction(self, sk):
+        return self.classifier_sk(self.sketch_model(sk))
+
+    def get_image_prediction(self, im):
+        return self.classifier_im(self.image_model(im))
 
     def train_once(self, train_loader, epoch, args):
         # Switch to train mode

src/train.py
@@ -13,7 +13,7 @@ import torch.backends.cudnn as cudnn
 # user defined
 import utils
 from logger import Logger, AverageMeter
-from test import validate
+from test import validate, accuracy
 from models import get_model
 
 
@@ -96,9 +96,15 @@ def main():
             print('mAP@all on validation set after {0} epochs: {1:.4f} (real), {2:.4f} (binary)'
                 .format(epoch + 1, map_, np.mean(valid_data['aps@all_bin'])))
 
+            accuracy_per_epoch_time = time.time()
+            acc_data, acc_time_info = accuracy(train_loader, model, epoch, args)
+            time_info['accuracy_per_epoch_time'] = time.time() - accuracy_per_epoch_time
+
             if args.log_online:
                 for key in valid_data.keys():
                     valid_data[key] = np.mean(valid_data[key])
+                for key in acc_data.keys():
+                    valid_data[key] = acc_data[key]
                 wandb.log(valid_data)
 
             del valid_data
@@ -123,6 +129,8 @@ def main():
             if args.log_online:
                 for key in valid_time_info.keys():
                     time_info[key] = valid_time_info[key]
+                for key in acc_time_info.keys():
+                    time_info[key] = acc_time_info[key]
                 wandb.log(time_info)
 
     # load the best model yet

src/utils.py
@@ -30,10 +30,30 @@ def numeric_classes(tags_classes, dict_tags):
     return num_classes
 
 
-def create_dict_texts(texts):
-    texts = sorted(list(set(texts)))
-    d = {l: i for i, l in enumerate(texts)}
-    return d
+def create_dict_texts(root, zero_version):
+    # training classes
+    class_to_int_path = os.path.join(root, zero_version, 'cname_cid.txt')
+    class_to_int_dict = {}
+    int_to_class_dict = {}
+    with open(class_to_int_path) as f:
+        for line in f:
+            cl, i = process_str(line)
+            i = int(i)
+            class_to_int_dict[cl] = i
+            int_to_class_dict[i] = cl
+    return class_to_int_dict, int_to_class_dict
+
+
+@staticmethod
+def process_str(line):
+    contents = line.split()
+    val = contents[-1]
+    name = []
+    for c in contents[0:-1]:
+        name += c.split('-')
+    join_symbol = '_'
+    key = join_symbol.join(name)
+    return key, val
 
 
 def read_config():
@@ -195,9 +215,6 @@ def load_files_sketchy_zeroshot(root_path, split_eccv_2018=False, filter_sketch=
         # Make train and validation splits randomly
         tr_classes = np.random.choice(np.setdiff1d(classes, te_classes), int(0.75 * len(classes)), replace=False)
         va_classes = np.setdiff1d(classes, np.union1d(tr_classes, te_classes))
-        # tr_classes = np.random.choice(classes, int(0.8 * len(classes)), replace=False)
-        # va_classes = np.random.choice(np.setdiff1d(classes, tr_classes), int(0.1 * len(classes)), replace=False)
-        # te_classes = np.setdiff1d(classes, np.union1d(tr_classes, va_classes))
 
     train_paired = False if model == 'sake' else True
     idx_tr_im, idx_tr_sk = get_coarse_grained_samples(tr_classes, fls_im, fls_sk, paired=train_paired,
@@ -256,9 +273,11 @@ def load_files_tuberlin_zeroshot(root_path, photo_dir='images', sketch_dir='sket
     te_classes = []
     with open(test_path) as f:
         for line in f:
-            content = line.split()[0].split('/')
-            if not content[1] in te_classes:
-                te_classes.append(content[1])
+            join_symbol = '_'
+            content = join_symbol.join(line.split('/')[1].split())
+            content = join_symbol.join(content.split('-'))
+            if content not in te_classes:
+                te_classes.append(content)
     te_classes = np.asarray(te_classes)
     # get train and validation classes
     if model == 'sake':
@@ -284,9 +303,6 @@ def load_files_tuberlin_zeroshot(root_path, photo_dir='images', sketch_dir='sket
         # make train/validation splits randomly
         tr_classes = np.random.choice(np.setdiff1d(classes, te_classes), int(0.80 * len(classes)), replace=False)
         va_classes = np.setdiff1d(classes, np.union1d(tr_classes, te_classes))
-        # tr_classes = np.random.choice(classes, int(0.88 * len(classes)), replace=False)
-        # va_classes = np.random.choice(np.setdiff1d(classes, tr_classes), int(0.06 * len(classes)), replace=False)
-        # te_classes = np.setdiff1d(classes, np.union1d(tr_classes, va_classes))
 
     train_paired = False if model == 'sake' else True
     idx_tr_im, idx_tr_sk = get_coarse_grained_samples(tr_classes, fls_im, fls_sk, paired=train_paired)
@@ -474,32 +490,28 @@ def get_datasets(args):
         splits['te_clss_im'] = np.concatenate((tr_clss_im_[idx_im], splits['te_clss_im']), axis=0)
 
     # class dictionary
-    args.dict_clss = create_dict_texts(splits['tr_clss_im'])
-    cid_mask = True if 'sake' in args.model else False
+    args.dict_clss_str2int, args.dict_clss_int2str = create_dict_texts(args.root_path, args.zero_version)
+
     if 'sake' in args.model:
         data_train_sketch = DataGeneratorSketch(args.dataset, args.root_path, sketch_dir, sketch_sd, splits['tr_fls_sk'],
-                                            splits['tr_clss_sk'], transforms=transform_sketch, cid_mask=cid_mask,
-                                            zero_version=args.zero_version)
+                                                splits['tr_clss_sk'], transforms=transform_sketch,
+                                                int2str=args.dict_clss_int2str, zero_version=args.zero_version)
         data_train_image = DataGeneratorImage(args.dataset, args.root_path, photo_dir, photo_sd, splits['tr_fls_im'],
-                                          splits['tr_clss_im'], transforms=transform_image, cid_mask=cid_mask,
-                                          zero_version=args.zero_version)
+                                              splits['tr_clss_im'], transforms=transform_image,
+                                              int2str=args.dict_clss_int2str, zero_version=args.zero_version)
         data_train = (data_train_image, data_train_sketch)
     else:
         data_train = DataGeneratorPaired(args.dataset, args.root_path, photo_dir, sketch_dir, photo_sd, sketch_sd,
                                          splits['tr_fls_sk'], splits['tr_fls_im'], splits['tr_clss_im'],
                                          transforms_sketch=transform_sketch, transforms_image=transform_image)
     data_valid_sketch = DataGeneratorSketch(args.dataset, args.root_path, sketch_dir, sketch_sd, splits['va_fls_sk'],
-                                            splits['va_clss_sk'], transforms=transform_sketch, cid_mask=False,
-                                            zero_version=args.zero_version)
+                                            splits['va_clss_sk'], transforms=transform_sketch)
     data_valid_image = DataGeneratorImage(args.dataset, args.root_path, photo_dir, photo_sd, splits['va_fls_im'],
-                                          splits['va_clss_im'], transforms=transform_image, cid_mask=False,
-                                          zero_version=args.zero_version)
+                                          splits['va_clss_im'], transforms=transform_image)
     data_test_sketch = DataGeneratorSketch(args.dataset, args.root_path, sketch_dir, sketch_sd, splits['te_fls_sk'],
-                                           splits['te_clss_sk'], transforms=transform_sketch, cid_mask=False,
-                                           zero_version=args.zero_version)
+                                           splits['te_clss_sk'], transforms=transform_sketch)
     data_test_image = DataGeneratorImage(args.dataset, args.root_path, photo_dir, photo_sd, splits['te_fls_im'],
-                                         splits['te_clss_im'], transforms=transform_image, cid_mask=False,
-                                         zero_version=args.zero_version)
+                                         splits['te_clss_im'], transforms=transform_image)
     print('Done')
 
     if not isinstance(data_train, DataGeneratorPaired):
@@ -554,7 +566,7 @@ def get_params(args):
     params_model['dim_out'] = args.dim_out
     params_model['sem_dim'] = args.sem_dim
     # Number of classes
-    params_model['num_clss'] = len(args.dict_clss)
+    params_model['num_clss'] = len(args.dict_clss_str2int)
     # Weight (on losses) parameters
     params_model['lambda_se'] = args.lambda_se
     params_model['lambda_im'] = args.lambda_im
@@ -575,7 +587,8 @@ def get_params(args):
     # Files with semantic labels
     params_model['files_semantic_labels'] = args.files_semantic_labels
     # Class dictionary
-    params_model['dict_clss'] = args.dict_clss
+    params_model['dict_clss_int2str'] = args.dict_clss_int2str
+    params_model['dict_clss_str2int'] = args.dict_clss_str2int
     # SAKE specific parameters
     if "sake" in args.model:
         params_model['arch'] = args.arch